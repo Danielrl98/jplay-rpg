f625e9fc8e559546b7e44dae76f818cbf49e0fe6
daniel-judit <daniel.resende@judit.io>
2025-01-19 16:50:09 -0300

    fix: ajustes build

--- build.cmd
+++ build.cmd
@@ -1,5 +1,11 @@
 javac -d bin -cp "libs/*" src/jogo/*.java  
 
+@REM java -jar cfr.jar libs/jplay.jar --outputdir ./bin/jplay
+
+jar xf libs/jplay.jar
+
+move jplay bin
+
 jar cfm target/app.jar MANIFEST.MF -C bin .
 
 java -jar target/app.jar
